b322fe1b13ed75bd55adb73405bd4384c653ce5f
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2023-10-27 14:33:18 -0400

    Delete and rework

--- src/device_init.cpp
+++
@@ -1,8 +0,0 @@
-#include <dragon/audio.hpp>
-
-DGAPI DgResult dgCreateAudioDevice(std::shared_ptr<DgAudioDevice> pAudioDevice, ALCchar* pName) {
-	alGetError();
-	pAudioDevice->device = alcOpenDevice(pName);
-	pAudioDevice->context = alcCreateContext(pAudioDevice->device, NULL);
-	return DG_SUCCESS;
-}
